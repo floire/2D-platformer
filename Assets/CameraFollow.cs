04303d891f7086cd428f35df8337a972561c7456
floire <48097955+floire@users.noreply.github.com>
2020-04-23 22:01:17 +0200

    Initial commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform mainFollow;
+    public Transform spiritFollow;
+    public float CameraDistance = 30.0f;
+
+    void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / CameraDistance);
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(mainFollow.position.x, mainFollow.position.y + 2, transform.position.z);
+    }
+}
+
---
+++ Assets/PlayerSwitch.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwitch : MonoBehaviour
+{
+    public int count = 1;
+    public GameObject mainPlayer;
+    public GameObject spiritPlayer;
+
+
+
+    void Start()
+    {
+        mainPlayer.GetComponent<PlayerController>().enabled = true;
+        spiritPlayer.GetComponent<PlayerController>().enabled = false;
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (count == 1)
+            {
+                mainPlayer.GetComponent<PlayerController>().enabled = false;
+                spiritPlayer.GetComponent<PlayerController>().enabled = true;
+                count = 0;
+
+
+            }
+            else
+            {
+                mainPlayer.GetComponent<PlayerController>().enabled = true;
+                spiritPlayer.GetComponent<PlayerController>().enabled = false;
+                count = 1;
+            }
+        }
+    }
+}
